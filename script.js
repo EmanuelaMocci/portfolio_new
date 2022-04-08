eaf573d05f6d361da7cc253b1d35d4eb3623255d
EmanuelaMocci <emanuelamocci2@gmail.com>
2022-04-08 16:04:29 +0200

    Add: cursor style with javascript

--- script.js
+++ script.js
@@ -16,3 +16,17 @@ function typeWriter() {
 }
 
 // /intro
+
+// cursor
+const cursorPointed = document.querySelector(".pointed");
+
+const moveCursor = (e) => {
+  const mouseY = e.clientY;
+  const mouseX = e.clientX;
+
+  cursorPointed.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
+};
+
+window.addEventListener("mousemove", moveCursor);
+
+// /cursor
